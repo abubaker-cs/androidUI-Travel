191a5c977ea2334efa2add308c6557a0857278d3
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-05-23 01:17:10 +0500

    Working on the login screen

---
+++ app/src/main/java/org/abubaker/travel/SplashScreen.kt
@@ -0,0 +1,34 @@
+package org.abubaker.travel
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+                }
+            }
+        }
+        thread.start()
+
+    }
+}
