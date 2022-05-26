152373ecd0cc3950ff62b0b4867aa6f23fe3b564
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-05-27 00:14:08 +0500

    Next: Utilize navGraph

--- app/src/main/java/org/abubaker/travel/Login.kt
+++ app/src/main/java/org/abubaker/travel/Login.kt
@@ -1,11 +1,40 @@
 package org.abubaker.travel
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.abubaker.travel.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Create an Account
+        binding.btnCreateAccount.setOnClickListener {
+
+            val i = Intent(this@Login, Register::class.java)
+            startActivity(i)
+
+        }
+
+        // Sign in
+        binding.btnSignIn.setOnClickListener {
+
+            val i = Intent(this@Login, MainActivity::class.java)
+            startActivity(i)
+
+        }
+
+
     }
+
+
 }
--- app/src/main/java/org/abubaker/travel/Register.kt
+++ app/src/main/java/org/abubaker/travel/Register.kt
@@ -1,11 +1,34 @@
 package org.abubaker.travel
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.abubaker.travel.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Back
+        binding.btnBack.setOnClickListener {
+
+            val i = Intent(this@Register, Login::class.java)
+            startActivity(i)
+
+        }
+
     }
+
+    // Continue:
+    // https://www.youtube.com/watch?v=D63bgYexWqM&t=1068s&ab_channel=Adityatprtma
+
+
 }
